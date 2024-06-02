35107d82e40fce72e5c304f0f50bb69ff705dcea
Uttam Dutta <uttamdutta7272@gmail.com>
2024-06-02 20:45:37 +0530

    link added

--- constants/index.ts
+++ constants/index.ts
@@ -1,50 +1,55 @@
 // NAVIGATION
 export const NAV_LINKS = [
-    { href: '/', key: 'home', label: 'Home' },
-    { href: '/about', key: 'about', label: 'About' },
-    { href: '/services', key: 'services', label: 'Services' },
-    { href: '/contact_us', key: 'contact_us', label: 'Contact Us' },
-  ];
-  
-  // SERVICES SECTION
-  export const SERVICES_URL = {
-    '1st':'/2nd_point.png',
-    '5th': '/4th_point.jpg',
-    '2nd':'/5th_point.jpg',
-    '3rd':'/7th_point.jpg',
-    '4th': '/10th_point.png',
-  };
-  
-  
-  // FOOTER SECTION
-  export const FOOTER_LINKS = [
-    {
-      title: 'Learn More',
-      links: [
-        {label:'About Aurora Group',link:'./about'},
-        {label:'Contact Us',link:'./contact_us'}
-        
-      ],
-    },
-  ];
-  
-  export const FOOTER_CONTACT_INFO = {
-    title: 'Contact Us',
-    links: [
-      { label: 'Admin', value: 'Shatrujeet Singh' },
-      { label: 'Email', value: 'aurora.group@gmail.com' },
-      {label: 'Contact No.', value: '+91 72729 74388' },
-    ],
-  };
-  
-  export const SOCIALS = {
-    title: 'Social',
-    links: [
-      '/facebook.svg',
-      '/instagram.svg',
-      '/twitter.svg',
-      '/youtube.svg',
-      '/wordpress.svg',
-    ],
-  };
-  
+	{ href: '/', key: 'home', label: 'Home' },
+	{ href: '/about', key: 'about', label: 'About' },
+	{ href: '/services', key: 'services', label: 'Services' },
+	{ href: '/contact_us', key: 'contact_us', label: 'Contact Us' }
+]
+
+// SERVICES SECTION
+export const SERVICES_URL = {
+	'1st': '/2nd_point.png',
+	'5th': '/4th_point.jpg',
+	'2nd': '/5th_point.jpg',
+	'3rd': '/7th_point.jpg',
+	'4th': '/10th_point.png'
+}
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+	{
+		title: 'Learn More',
+		links: [
+			{ label: 'About Aurora Group', link: './about' },
+			{ label: 'Contact Us', link: './contact_us' }
+		]
+	}
+]
+
+export const FOOTER_CONTACT_INFO = {
+	title: 'Contact Us',
+	links: [
+		{ label: 'Admin', value: 'Shatrujeet Singh' },
+		{ label: 'Email', value: 'aurora.group@gmail.com' },
+		{ label: 'Contact No.', value: '+91 72729 74388' }
+	]
+}
+
+export const SOCIALS = {
+	title: 'Social',
+	links: [
+		'/facebook.svg',
+		'/instagram.svg',
+		'/twitter.svg',
+		'/youtube.svg'
+		// '/wordpress.svg'
+	]
+}
+
+export const LINKS = [
+	'https://www.facebook.com/profile.php?id=61560120154673&mibextid=LQQJ4d',
+	'https://www.instagram.com/auroragroup321?igsh=MWxldW9lNWhzbnc5cA%3D%3D&utm_source=qr',
+	'/',
+	'/',
+	'/'
+]
